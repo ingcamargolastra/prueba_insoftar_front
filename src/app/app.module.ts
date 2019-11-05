e021f76788a1451c6d8baf3b0dac084e50d7e7ec
Desarrollo Tamaca <desarrollotamaca@gmail.com>
2019-11-05 08:03:17 -0500

    Configuración de Angular Material OK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatDialogModule} from '@angular/material/dialog';
 
 import { AppComponent } from './app.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
@@ -15,7 +17,9 @@ import { CrearUsuarioComponent } from './usuarios/crear-usuario/crear-usuario.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDialogModule
   ],
   providers: [UsuarioService],
   bootstrap: [AppComponent]
